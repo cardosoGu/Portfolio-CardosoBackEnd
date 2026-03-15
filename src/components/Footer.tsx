567a421db666b1c84a5d7d2f41ec9bc2c2318ff4
Gustavo Cardoso <158459269+cardosoGu@users.noreply.github.com>
2026-03-15 12:55:36 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -22,11 +22,9 @@ export default function Footer() {
             <span className="text-blue-500">{'<'}</span>C
             <span className="text-blue-500">{'/>'}</span>
           </a>
-
           <p className="text-sm text-zinc-500 flex items-center gap-1.5">
-            {© {year}. {t('footer.copy')}
+            © {year}. {t('footer.copy')}
           </p>
-
           <a
             href="#"
             onClick={(e) => {
